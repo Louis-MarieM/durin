8e3aa288051756d4eae9e4e4a01ea8326fccfdab
Louis-MarieM <lmmaitrepierre@gmail.com>
2025-11-15 09:44:31 +0100

    Add TU extract ohlcv use case

--- src/durin/application/__init__.py
+++ src/durin/application/__init__.py
@@ -2,11 +2,13 @@ from .exceptions.application_exceptions import ApplicationException
 from .ports.inbound.use_cases.extraction.extract_ohlcv_use_case import ExtractOHLCVUseCase
 from .ports.outbound.providers.ohlcv_provider import OHLCVProvider
 from .use_cases.extraction.extract_ohlcv.extract_ohlcv_input_data import ExtractOHLCVInputData
+from .use_cases.extraction.extract_ohlcv.extract_ohlcv_output_data import ExtractOHLCVOutputData
 from .use_cases.extraction.extract_ohlcv.extract_ohlcv_use_case_impl import ExtractOHLCVUseCaseImpl
 
 __all__ = [
     "ApplicationException",
     "ExtractOHLCVInputData",
+    "ExtractOHLCVOutputData",
     "ExtractOHLCVUseCase",
     "ExtractOHLCVUseCaseImpl",
     "OHLCVProvider",
